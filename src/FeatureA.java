d1ed264e7e97a7bb9928324ae523c0fa2f3321ec
DipaliTalware <d.talware@gmail.com>
2024-09-16 10:27:54 +0200

    Changed FeatureA branch

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -1,8 +1,23 @@
-
+//Person A/B machen jeweils einen Commit für ihre Änderungen und pushen diese.
+//Person A :
+//Schaut euch die git commit History an (Log)
+//Vergleicht die commits von seinem eigenen Branch und dem main branch
+//Merged den main "into" euren Branch
+//Vergleicht erneut die commits von seinem eigenen Branch und dem main branch
+//Pusht den gemergeten branch
+//Stelle über github einen PullRequest und merged diesen.
+//Person B :
+//Update dein Projekt
+//Vergleicht die commits von seinem eigenen Branch und dem main branch
+//Merged den main "into" euren Branch
+//Ihr solltet jetzt eine "Merge Conflict bekommen", löst ihn mit hilfe von IntelliJ.
+//Vergleicht erneut die commits von seinem eigenen Branch und dem main branch
+//Pusht den gemergeten branch
+//Stelle über github einen PullRequest und merged diesen.
 
 public class FeatureA{
 
     public static void main(String[] args) {
-        System.out.println("Hallo, das ist der Code von <Name>");
+        System.out.println("Hallo, das ist der Code von Dipali");
     }
 }
